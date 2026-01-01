62298bf070b3121eac775c9581a78b32a453159e
jennifer hodge <jennifer.hodge@russdarrow.com>
2026-01-01 07:26:02 -0600

    done input labels

--- Tip-Calculator/src/Calculator.jsx
+++ Tip-Calculator/src/Calculator.jsx
@@ -12,9 +12,41 @@ export default function Calculator() {
         <div className="inputContainer">
           <div className="billAmount">
             <label htmlFor="bill-amount">Bill Amount</label>
-            <input type="number" id="bill-amount" name="bill-amount" className="amount" value="" />
+            <input
+              type="number"
+              id="bill-amount"
+              name="bill-amount"
+              className="amount"
+              value=""
+            />
+          </div>
+          <div className="tipPercent">
+            <label htmlFor="tip-percent">Tip Percentage</label>
+            <input
+              type="number"
+              id="tip-percent"
+              name="tip-percentage"
+              className="tips"
+              value=""
+            />
+          </div>
+          <div className="guestNumber">
+            <label htmlFor="guestNumber">Number of People</label>
+            <input
+              type="number"
+              id="guestNumber"
+              name="guestAmount"
+              className="peopleAmount"
+              value=""
+            />
           </div>
         </div>
+        <div className="calculateButton">
+          <button type="button" onClick="" className="calButton">
+            Calculate
+          </button>
+        </div>
+        <div className="underline"></div>
       </div>
     </>
   );
